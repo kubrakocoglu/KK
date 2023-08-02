c40970002a62b42a67b68576884e9eccc7d181ca
kubrakocoglu <139239059+kubrakocoglu@users.noreply.github.com>
2023-08-02 17:29:14 +0300

    Add files via upload

---
+++ sınavh.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+enum color{red,white,blue=3,green,black=-1
+};
+int main()
+{
+	enum color clo;
+	clo=green;
+	printf("%d%d%d%d",black,red,white,clo);
+	return 0;
+}
+
